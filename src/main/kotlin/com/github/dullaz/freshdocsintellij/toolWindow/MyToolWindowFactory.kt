32fc5735887a4804e778ea2e1b585a438b7b7719
GitHub Action <action@github.com>
2025-08-16 21:06:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/dullaz/freshdocsintellij/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/dullaz/freshdocsintellij/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.dullaz.freshdocsintellij.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.dullaz.freshdocsintellij.MyBundle
+import com.github.dullaz.freshdocsintellij.services.MyProjectService
 import javax.swing.JButton
 
 
